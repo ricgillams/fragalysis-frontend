90e6c09be54493a0fea60be08a60efb4e1a8e151
Ric Gillams <ric.gillams@gmail.com>
2018-08-02 18:16:22 +0100

    adding molecule list to fragglebox

--- js/components/updateOrientation.js
+++ js/components/updateOrientation.js
@@ -33,7 +33,7 @@ export class UpdateOrientation extends React.Component {
             this.props.setTargetOn(targetOn);
             var molGroupList = JSON.parse(JSON.parse(myJson.scene)).molGroupList;
             // this.props.setMolGroupList(molGroupList);
-            var molGroupOn = JSON.parse(JSON.parse(myJson.scene)).mol_group_on;
+            var mol_group_on = JSON.parse(JSON.parse(myJson.scene)).mol_group_on;
             this.props.setMolGroupOn(mol_group_on);
         }
     };
