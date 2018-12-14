f36afefe4496303e27287d1ac5d4ec9614347c19
Ric Gillams <ric.gillams@gmail.com>
2018-12-14 13:44:09 +0000

    add links to SGC page

--- js/components/targetList.js
+++ js/components/targetList.js
@@ -28,8 +28,8 @@ class TargetList extends GenericList {
         return <ListGroupItem key={data.id} >
             <Link to={preview} styles={{float: 'left'}}>{data.title}</Link>
             {/*<span className="myClass" styles={{float : 'left', paddingRight : '5px'}} > </span>*/}
-            <div style={{display: 'inline-block'}}></div>
-            {/*<p styles={{display: 'inline'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>*/}
+            {/*<div style={{display: 'inline-block'}}></div>*/}
+            <p style={{display: 'inline'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
             <a href={sgcUrl} target="new" styles={{float: 'right'}}>Open SGC summary</a>
         </ListGroupItem>
     }
