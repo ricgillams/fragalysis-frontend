021864d3cca624ccb69ed50d0de973c0a4db3dfd
Ric Gillams <ric.gillams@gmail.com>
2018-12-17 13:59:56 +0000

    remove nbsp

--- js/components/downloadPdb.js
+++ js/components/downloadPdb.js
@@ -30,12 +30,7 @@ class DownloadPdb extends React.Component {
         for (var structure in protInfo) {
             var pdbData = pdbInfo[structure].bound_pdb_data;
             var pdbCode = protInfo[structure].code;
-            // var molGroupUrl = window.location.protocol + "//" + window.location.host + "/api/molecules/?prot_id=" + pdbInfo[0].id;
-            // const molResponse = await fetch(molGroupUrl);
-            // const molJson = await molResponse.json();
-            // const sdfData = molJson.results[0].sdf_info;
             totFolder.file(pdbCode + ".pdb", pdbData);
-            // totFolder.file(pdbCode + ".sdf", sdfData);
         }
         const content = await zip.generateAsync({type: "blob"});
         FileSaver.saveAs(content, fName + ".zip");
